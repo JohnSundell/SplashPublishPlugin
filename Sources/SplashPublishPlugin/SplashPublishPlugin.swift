683a775493a4ee219f48ca63f7f299dbc68be659
Joe Diragi <joyod3@gmail.com>
2022-08-12 16:37:18 -0400

    Implements custom grammars functionality

--- Sources/SplashPublishPlugin/SplashPublishPlugin.swift
+++ Sources/SplashPublishPlugin/SplashPublishPlugin.swift
@@ -9,19 +9,21 @@ import Splash
 import Ink
 
 public extension Plugin {
-    static func splash(withClassPrefix classPrefix: String) -> Self {
+    /// - withClassPrefix: A string to be appended to each HTML tag's class attribute
+    /// - withGrammars: A tuple containing the `Grammar` itself as well as the tag to look for inside of markdown. 
+    static func splash(withClassPrefix classPrefix: String, withGrammars grammars: [(grammar: Grammar, name: String)] = [(SwiftGrammar(), "swift")]) -> Self {
         Plugin(name: "Splash") { context in
             context.markdownParser.addModifier(
                 .splashCodeBlocks(withFormat: HTMLOutputFormat(
                     classPrefix: classPrefix
-                ))
+                ), withGrammars: grammars)
             )
         }
     }
 }
 
 public extension Modifier {
-    static func splashCodeBlocks(withFormat format: HTMLOutputFormat = .init()) -> Self {
+    static func splashCodeBlocks(withFormat format: HTMLOutputFormat = .init(), withGrammars grammars: [(grammar: Grammar, name: String)] = [(SwiftGrammar(), "swift")]) -> Self {
         var highlighter = SyntaxHighlighter(format: format)
 
         return Modifier(target: .codeBlocks) { html, markdown in
@@ -31,9 +33,11 @@ public extension Modifier {
                 return html
             }
             
-            if markdown.hasPrefix("yml") || markdown.hasPrefix("yaml") {
-                highlighter = SyntaxHighlighter(format: format, grammar: YamlGrammar())
-            }
+            grammars.forEach({ grammar, name in
+                if markdown.hasPrefix(name) {
+                    highlighter = SyntaxHighlighter(format: format, grammar: grammar)
+                }
+            })
 
             markdown = markdown
                 .drop(while: { !$0.isNewline })
--- Tests/SplashPublishPluginTests/SplashPublishPluginTests.swift
+++ Tests/SplashPublishPluginTests/SplashPublishPluginTests.swift
@@ -7,6 +7,7 @@
 import XCTest
 import SplashPublishPlugin
 import Ink
+import Splash
 
 final class SplashPublishPluginTests: XCTestCase {
     func testHighlightingMarkdown() {
@@ -28,8 +29,78 @@ final class SplashPublishPluginTests: XCTestCase {
         </code></pre>
         """)
     }
+    
+    func testHighlightingMultipleGrammarsMarkdown() {
+        let parser = MarkdownParser(modifiers: [.splashCodeBlocks(withGrammars: [(TestGrammar(), "test"), (SwiftGrammar(), "swift")])])
+        let html = parser.html(from: """
+        Some text
+        ```test
+        some should be a keyword
+        ```
+        here's some fake text
+        ```swift
+        let int = 7
+        ```
+        fake text
+        ```no-highlight
+        not.highlighted()
+        ```
+        """)
+        XCTAssertEqual(html, """
+        <p>Some text</p><pre><code><span class=\"keyword\">some</span> should be a keyword\n</code></pre><p>here\'s some fake text</p><pre><code><span class=\"keyword\">let</span> int = <span class=\"number\">7</span>\n</code></pre><p>fake text</p><pre><code class=\"language-no-highlight\">not.highlighted()\n</code></pre>
+        """)
+    }
 
     static var allTests = [
         ("testHighlightingMarkdown", testHighlightingMarkdown)
     ]
+}
+
+public struct TestGrammar: Grammar {
+    public var delimiters: CharacterSet
+    public var syntaxRules: [SyntaxRule]
+    
+    public init() {
+        var delimiters = CharacterSet.alphanumerics.inverted
+        delimiters.remove("_")
+        delimiters.remove("-")
+        delimiters.remove("\"")
+        delimiters.remove("#")
+        delimiters.remove("@")
+        delimiters.remove("$")
+        self.delimiters = delimiters
+        
+        syntaxRules = [
+            KeywordRule(),
+        ]
+    }
+    
+    public func isDelimiter(_ delimiterA: Character, mergableWith delimiterB: Character) -> Bool {
+        switch (delimiterA, delimiterB) {
+        case (_, ":"):
+            return false
+        case (":", "/"):
+            return true
+        case (":", _):
+            return false
+        case ("-", _):
+            return false
+        case ("#", _):
+            return false
+        default:
+            return true
+        }
+    }
+    
+    static let keywords = ([
+        "some", "fake", "keywords"
+    ] as Set<String>)
+    
+    struct KeywordRule: SyntaxRule {
+        var tokenType: TokenType { return .keyword }
+        
+        func matches(_ segment: Segment) -> Bool {
+            return keywords.contains(segment.tokens.current)
+        }
+    }
 }
